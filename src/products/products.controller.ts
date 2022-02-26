2f36eacb3e4b81f27b2d041f2b9484287b142f66
Tom <tomadm@817465-cn54833.tmweb.ru>
2022-02-26 19:37:55 +0300

    First commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Param, Post, Delete, Put, Body, Redirect } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+
+@Controller('products')
+export class ProductsController {
+    @Get()
+    @Redirect('https://google.com', 301)
+    getAll() : string {
+        return 'getAll'
+    }
+
+    @Get(':id')
+    getOne(@Param('id') id:string) {
+        return 'getOne' + id
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    @Header('Cache-control', 'none')
+    create(@Body() createProductDto: CreateProductDto): string {
+        return `Title: ${createProductDto.title} Price: ${createProductDto.price}`
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `Remove  ${id}`
+    }
+
+    @Put(':id')
+    update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
+        return 'Update ' + id
+    }
+
+
+}
